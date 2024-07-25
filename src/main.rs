b60b1520d59fc27d090c3216cd31455cbe051f14
Chris Davison <c.jr.davison@gmail.com>
2024-07-25 15:11:18 +0100

    remove gitpath. tidy untracked impl and repo parsing.

--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,6 @@ use shellexpand::tilde;
 
 mod git;
 
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone)]
-pub enum GitPath {
-    Keep(PathBuf),
-    Ignore(PathBuf),
-}
-
-impl GitPath {
-    pub fn to_path_buf(&self) -> PathBuf {
-        match self {
-            GitPath::Keep(p) => p.to_path_buf(),
-            GitPath::Ignore(p) => p.to_path_buf(),
-        }
-    }
-
-    pub fn keep(&self) -> Option<PathBuf> {
-        match self {
-            GitPath::Keep(p) => Some(p.to_path_buf()),
-            GitPath::Ignore(_) => None,
-        }
-    }
-
-    pub fn ignore(&self) -> Option<PathBuf> {
-        match self {
-            GitPath::Ignore(p) => Some(p.to_path_buf()),
-            GitPath::Keep(_) => None,
-        }
-    }
-}
-
-impl std::fmt::Display for GitPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        match self {
-            GitPath::Keep(p) => write!(f, "{}", p.display()),
-            GitPath::Ignore(p) => write!(f, "! {}", p.display()),
-        }
-    }
-}
-
 #[derive(Debug, StructOpt)]
 #[structopt(name = "repoutil", about = "Operations on multiple git repos")]
 struct Opts {
@@ -57,7 +19,7 @@ struct Opts {
     json: bool,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, StructOpt, PartialEq)]
 enum OptCommand {
     /// Push commits
     #[structopt(alias = "p")]
@@ -86,6 +48,9 @@ enum OptCommand {
 }
 
 fn common_substring<T: ToString>(ss: &[T]) -> String {
+    if ss.len() == 1 {
+        return String::new();
+    }
     let mut idx = 0;
     let charlists = ss
         .iter()
@@ -108,24 +73,6 @@ fn common_substring<T: ToString>(ss: &[T]) -> String {
     ss[0].to_string().chars().take(idx).collect()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::common_substring;
-
-    #[test]
-    fn test_common_substring() {
-        assert_eq!(common_substring(&["aaa", "aab", "aac"]), "aa");
-        assert_eq!(common_substring::<&str>(&[]), "");
-        assert_eq!(
-            common_substring(&[
-                "/home/cdavison/code",
-                "/home/cdavison/code/recipes",
-                "/home/cdavison/strathclyde"
-            ]),
-            "/home/cdavison/"
-        );
-    }
-}
 
 fn main() {
     let opts = Opts::from_args();
@@ -143,55 +90,56 @@ fn main() {
         OptCommand::Untracked => git::untracked,
     };
 
-    let all_repos = get_repos_from_config().expect("Couldn't get repos");
+    let (includes, excludes) = get_repos_from_config().expect("Couldn't get repos");
+    let repos = if opts.command == OptCommand::Untracked { excludes } else { includes };
 
     let common = common_substring(
-        &all_repos
+        &repos
             .iter()
-            .map(|x| format!("{}", x))
+            .map(|x| format!("{}", x.display()))
             .collect::<Vec<String>>(),
     );
-    let mut outs = Vec::new();
-    let out: Vec<_> = all_repos
+    let outs: Vec<_> = repos
         .par_iter()
-        .progress_count(all_repos.len() as u64)
-        .map(|repo| cmd(repo, json))
-        .collect();
-    for output in out {
-        match output {
+        .progress_count(repos.len() as u64)
+        .filter_map(|repo| match cmd(repo, json) {
             Ok(Some(thing)) => {
-                let thing = thing.replace(&common, "");
-                if json {
-                    outs.push(thing);
-                } else {
-                    println!("{}", thing)
-                }
+                Some(thing.replace(&common, ""))
             }
-            Ok(_) => (),
-            Err(e) => eprintln!("ERR: {}", e),
-        }
-    }
+            Ok(_) => None,
+            Err(e) => {
+                eprintln!("ERR `{}`: {}", repo.display(), e);
+                None
+            },
+        })
+        .collect();
     if json {
         println!("{{\"items\": [{}]}}", outs.join(","));
+    } else {
+        println!("{}", outs.join("\n"));
     }
 }
 
-fn get_dirs_from_config() -> Result<(Vec<GitPath>, Vec<GitPath>)> {
+fn get_dirs_from_config() -> Result<(Vec<PathBuf>, Vec<PathBuf>)> {
     let repoutil_config = tilde("~/.repoutilrc").to_string();
     let p = std::path::Path::new(&repoutil_config);
 
     if !p.exists() {
-        return Err(anyhow!("No ~/.repoutilrc, or passed dirs"));
+        return Err(anyhow!("No ~/.repoutilrc"));
     }
 
     let mut includes = Vec::new();
     let mut excludes = Vec::new();
     for line in std::fs::read_to_string(p)?.lines() {
         if let Some(stripped) = line.strip_prefix('!') {
+            let path = PathBuf::from(tilde(&stripped).to_string());
             // Strip 'exclusion-marking' ! from start of path, and add to excludes list
-            excludes.push(GitPath::Ignore(PathBuf::from(tilde(stripped).to_string())));
+            excludes.push(path);
         } else {
-            includes.push(GitPath::Keep(PathBuf::from(tilde(&line).to_string())));
+            let path = PathBuf::from(tilde(&line).to_string());
+            if !excludes.contains(&path) {
+                includes.push(path);
+            }
         }
     }
     Ok((includes, excludes))
@@ -208,28 +156,19 @@ fn get_repos_from_dir(dir: &Path) -> Result<Vec<PathBuf>> {
     Ok(repos)
 }
 
-fn get_repos_from_config() -> Result<Vec<GitPath>> {
+fn get_repos_from_config() -> Result<(Vec<PathBuf>, Vec<PathBuf>)> {
     let (inc, exc) = get_dirs_from_config()?;
-    let mut all_repos = Vec::new();
-    for dir in inc {
-        let dir = dir.keep().unwrap();
-        if git::is_git_repo(&dir) {
-            all_repos.push(GitPath::Keep(dir));
-        } else {
-            let repos = match get_repos_from_dir(&dir) {
-                Ok(r) => r,
-                Err(e) => {
-                    eprintln!("Couldn't get repos from '{:?}': '{}'\n", dir, e);
-                    continue;
-                }
-            };
-            all_repos.extend(repos.iter().map(|p| GitPath::Keep(p.to_path_buf())));
-        }
-    }
+    let mut excludes = Vec::with_capacity(exc.len());
     for dir in exc {
-        let dir = dir.ignore().unwrap();
         if git::is_git_repo(&dir) {
-            all_repos.push(GitPath::Ignore(dir));
+            excludes.push(dir);
+        }
+    }
+
+    let mut includes = Vec::with_capacity(inc.len());
+    for dir in inc {
+        if git::is_git_repo(&dir) {
+            includes.push(dir);
         } else {
             let repos = match get_repos_from_dir(&dir) {
                 Ok(r) => r,
@@ -238,10 +177,32 @@ fn get_repos_from_config() -> Result<Vec<GitPath>> {
                     continue;
                 }
             };
-            all_repos.extend(repos.iter().map(|p| GitPath::Ignore(p.to_path_buf())));
+            includes.extend(repos.iter().map(|p| p.to_path_buf()));
         }
     }
-    all_repos.sort();
-    all_repos.dedup();
-    Ok(all_repos)
+        includes.sort();
+    includes.dedup();
+    let includes = includes.iter().filter(|x| !excludes.contains(x)).cloned().collect();
+    Ok((includes, excludes))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::common_substring;
+
+    #[test]
+    fn test_common_substring() {
+        assert_eq!(common_substring(&["aaa", "aab", "aac"]), "aa");
+        assert_eq!(common_substring::<&str>(&[]), "");
+        assert_eq!(common_substring(&["Something"]), "");
+        assert_eq!(
+            common_substring(&[
+                "/home/cdavison/code",
+                "/home/cdavison/code/recipes",
+                "/home/cdavison/strathclyde"
+            ]),
+            "/home/cdavison/"
+        );
+    }
+}
+
